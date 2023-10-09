75341829290963d59898718649aa1fd8a4810435
yoon0-0 <yoonbyung-chai@korea.ac.kr>
2023-10-09 18:50:54 +0900

    pass model to ray process

---
+++ code/amp/utils/constant.py
@@ -0,0 +1,11 @@
+# Humanoid
+
+DOF_BODY_IDS    = [1, 2, 4, 5, 6,
+                   7, 8, 9, 10,
+                   12, 13, 14,
+                   16, 17, 18
+                    ]    # body idx which have DOF, not include root
+DOF_OFFSETS     = [0, 3, 4, 7, 8, 11,
+                   14, 15, 18, 21,
+                   24, 25, 28,
+                   31, 32, 35]  # joint number offset of each body
