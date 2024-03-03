a68616482c8ea8ebce2facd7174a638bf870f133
yoon0-0 <yoonbyung-chai@korea.ac.kr>
2024-03-04 01:52:29 +0900

    fix contact reset error

--- code/mujoco_parser_ray.py
+++ code/mujoco_parser_ray.py
@@ -49,7 +49,7 @@ class MuJoCoParserClassRay(MuJoCoParserClass):
         self.env_id = env_id
         self.mode = mode
         self.horizon_length = horizon_length
-        self._contact_forces = torch.zeros((self.n_body, 3), device=self.device)
+        self._contact_forces = torch.zeros((self.n_body-1, 3), device=self.device)
         self._contact_body_ids = contact_body_ids
         self._key_body_ids = key_body_ids
         self.reset_flag = torch.ones((1,))
@@ -406,11 +406,13 @@ class MuJoCoParserClassRay(MuJoCoParserClass):
         
         trgt = None
         amp_obs = None
+        body_name = None
         
         for n in range(self.horizon_length):
             # reset actor
             # self.obs, done_env_ids = self._env_reset_done()
             if self.reset_flag[0]:
+                self._contact_forces = torch.zeros_like(self._contact_forces)
                 self.tick = torch.tensor(0)
                 motion_ids = self._motion_lib.sample_motions(1)
                 self.motion_time = self._motion_lib.sample_time(motion_ids)
@@ -464,7 +466,8 @@ class MuJoCoParserClassRay(MuJoCoParserClass):
             else: # Deterministic
                 trgt = self.res_dict['mus']
 
-            super().step(ctrl=trgt,nstep=nstep,ctrl_idxs=ctrl_idxs,INCREASE_TICK=INCREASE_TICK)
+            # super().step(ctrl=trgt,nstep=nstep,ctrl_idxs=ctrl_idxs,INCREASE_TICK=INCREASE_TICK)
+            super().step(ctrl=torch.zeros_like(trgt),nstep=nstep,ctrl_idxs=ctrl_idxs,INCREASE_TICK=INCREASE_TICK)
             # next root state
             # self.root_states[0] = torch.from_numpy(np.concatenate((self.get_p_body('base'), r2quat(self.get_R_body('base'))[[1,2,3,0]], self.get_qvel_joint('base')[0:3], self.get_qvel_joint('base')[3:6]), axis=-1))
             # self.root_states[0] = torch.from_numpy(np.concatenate((self.get_p_body('root'), r2quat(self.get_R_body('root'))[[1,2,3,0]], self.get_qvel_joint(0)[0:3], self.get_qvel_joint(0)[3:6]), axis=-1))
